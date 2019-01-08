017b74fea1995e43a35ca38163410a98c29edbcf
Bryan Magers <bmagers@users.noreply.github.com>
2019-01-08 15:48:35 -0800

    Updates

---
+++ precache-manifest.70986602e38f04f48a4eafe6f5db0f27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "29e045391aa9e9d9a3c7",
+    "url": "/clicky-game/static/js/main.29e04539.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/clicky-game/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "29e045391aa9e9d9a3c7",
+    "url": "/clicky-game/static/css/main.f10b4cf5.chunk.css"
+  },
+  {
+    "revision": "9e5748c12c6def774468ba6dd6a706c3",
+    "url": "/clicky-game/index.html"
+  }
+];
